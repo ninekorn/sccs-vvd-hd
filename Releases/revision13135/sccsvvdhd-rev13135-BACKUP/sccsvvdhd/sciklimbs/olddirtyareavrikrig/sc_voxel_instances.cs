74d67b8f6473c4ddfc7d622333388193f326c3af
nine <steve.chasse@gmail.com>
2023-04-30 12:56:44 -0400

    revision 13135 - calibration update - halfway there
    hey everyone, i was able to calibrate approx 75 resolutions on 150. It's all of the low voxel definition resolutions that i worked on but out the 75 resolutions i calibrated, i will review 13 of those as i am unsure, and wrote next to the resolutions "to review" in the script scgraphicssec.cs.
    
    steve chassé aka 9

---
+++ Releases/revision13135/sccsvvdhd-rev13135-BACKUP/sccsvvdhd/sciklimbs/olddirtyareavrikrig/sc_voxel_instances.cs
@@ -0,0 +1,52 @@
+using SharpDX;
+using Jitter.Dynamics;
+
+namespace sccs
+{
+    public class sc_voxel_instances : ITransform, IComponent
+    {
+        public ITransform transform { get; private set; }
+        IComponent ITransform.Component
+        {
+            get => component;
+        }
+        IComponent component;
+        RigidBody IComponent.rigidbody { get; set; }
+
+        SoftBody IComponent.softbody { get; set; }
+
+        public Vector3 virtualelbowdirrightpoint { get; set; }
+        public Vector3 forwarddirection { get; set; }
+        public Vector3 rightdirection { get; set; }
+        public Vector3 updirection { get; set; }
+
+
+        public Matrix _TEMPPIVOT { get; set; }
+        public Matrix _TEMPPOSITION { get; set; }
+        public Matrix _REALCENTERPOSITION { get; set; }
+
+        public Matrix _POSITION { get; set; }
+        public Matrix _ORIGINPOSITION { get; set; }
+        public Matrix _LASTPOSITION { get; set; }
+        public Matrix _LASTPOSITIONFORPHYSICS { get; set; }
+        public Vector3 _ELBOWPOSITION { get; set; }
+        public Vector3 _ELBOWCROSSVEC { get; set; }
+
+        public Vector3 _UPPERPIVOT { get; set; }
+        public Vector3 _LOWERPIVOT { get; set; }
+
+
+        public Matrix _SHOULDERROT { get; set; }
+        public float _ARMLENGTH { get; set; }
+
+        public Matrix current_pos;// { get; set; }
+        public Vector3 sc_last_frame_linearvelocity { get; set; }
+        public Vector3 sc_last_frame_angularvelocity { get; set; }
+
+        public sc_voxel_instances()
+        {
+            transform = this;
+            component = this;
+        }
+    }
+}
